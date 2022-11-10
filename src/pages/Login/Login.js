fdb610de2471df4eaecc191e5a4561ea98451f04
unknown <al2220120120@gmail.com>
2022-11-10 00:23:50 -0600

    Primer avance de aplicación

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Title from './components/Title/Title';
+
+
+const Login = () => {
+    return(
+        <div>
+            <Title />
+        </div>
+    )
+};
+
+export default Login;
---
+++ src/pages/Login/components/Title/Title.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Title = () => {
+    return(
+        <div>
+            Hola Ttitulo
+        </div>
+    )
+};
+
+export default Title;
